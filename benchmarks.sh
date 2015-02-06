cd206af180bd4b84d39998269626614d2b3ad952
Antoine Grondin <antoinegrondin@gmail.com>
2015-02-06 03:18:54 -0500

    uncomment benchmark script

--- benchmarks.sh
+++ benchmarks.sh
@@ -30,12 +30,9 @@ OWN_FILENAME="bench/raw/own_$OWN_VERSION.bench"
 
 CMP_FILENAME="bench/$OWN_VERSION_vs_heap"$GO_VERSION"_llrb-"$LLRB_VERSION
 
-# echo "Benchmarking container/heap ($GO_VERSION) and GoLLRB ($LLRB_VERSION)"
-# go test ./bench/_bench/. -test.bench . -benchmem -tags other > $OTHER_FILENAME
-# echo "Benchmarking datagen heap and balanced trees ($OWN_VERSION)"
-# go test ./bench/_bench/. -test.bench . -benchmem -tags own > $OWN_FILENAME
-
-
-
+echo "Benchmarking container/heap ($GO_VERSION) and GoLLRB ($LLRB_VERSION)"
+go test ./bench/_bench/. -test.bench . -benchmem -tags other > $OTHER_FILENAME
+echo "Benchmarking datagen heap and balanced trees ($OWN_VERSION)"
+go test ./bench/_bench/. -test.bench . -benchmem -tags own > $OWN_FILENAME
 
 benchcmp $OTHER_FILENAME $OWN_FILENAME > $CMP_FILENAME
